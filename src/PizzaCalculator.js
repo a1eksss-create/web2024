a915d61e1e0c92a25386c6dcbfaf5fc65e2e7c66
a1eksss-create <minecraftboytut@gmail.com>
2025-05-13 21:30:05 +0500

    lesson13

--- src/PizzaCalculator.js
+++ src/PizzaCalculator.js
@@ -27,53 +27,161 @@ class Pizza {
     this.selectedToppings = [];
   }
 
-  addTopping(topping) {
-    if (this.toppings[topping] && !this.selectedToppings.includes(topping)) {
-      this.selectedToppings.push(topping);
+  addTopping(toppingName) {
+    if (
+      this.toppings[toppingName] &&
+      !this.selectedToppings.includes(toppingName)
+    ) {
+      this.selectedToppings.push(toppingName);
     }
   }
 
-  removeTopping(topping) {
-    const index = this.selectedToppings.indexOf(topping);
+  removeTopping(toppingName) {
+    const index = this.selectedToppings.indexOf(toppingName);
     if (index !== -1) {
       this.selectedToppings.splice(index, 1);
     }
-  }
-
-  getToppings() {
-    return this.selectedToppings;
-  }
-
-  getSize() {
-    return this.size;
   }
 
   getStuffing() {
     return this.stuffing;
   }
 
-  calculatePrice() {
-    let priceStuffing = this.stuffings[this.stuffing].price;
-    let priceSize = this.sizes[this.size].price;
-    let price = priceStuffing + priceSize;
+  getSize() {
+    return this.size;
+  }
 
-    this.selectedToppings.forEach(function (topping) {
-      let priceTopping = this.toppings[topping].price;
-      if (typeof priceTopping === "object") {
-        price += priceTopping[this.size];
-      } else {
-        price += priceTopping;
-      }
-    }, this);
+  getToppings() {
+    return this.selectedToppings;
+  }
+
+  calculatePrice() {
+    let price =
+      this.stuffings[this.stuffing].price + this.sizes[this.size].price;
+
+    this.selectedToppings.forEach((toppingName) => {
+      const toppingPrice = this.toppings[toppingName].price;
+      price +=
+        typeof toppingPrice === "object"
+          ? toppingPrice[this.size]
+          : toppingPrice;
+    });
+
     return price;
   }
 
   calculateCalories() {
     let calories =
       this.stuffings[this.stuffing].calories + this.sizes[this.size].calories;
-    this.selectedToppings.forEach(function (topping) {
-      calories += this.toppings[topping].calories;
-    }, this);
+
+    this.selectedToppings.forEach((toppingName) => {
+      calories += this.toppings[toppingName].calories;
+    });
+
     return calories;
   }
 }
+
+let addToCartButton = document.getElementById("addToCart");
+
+let pizzaButtons = document.querySelectorAll(".pizza");
+let sizeButtons = document.querySelectorAll(".size");
+let toppingCheckboxes = document.querySelectorAll(".topping");
+
+let selectedStuffing = "Маргарита";
+let selectedSize = "Маленькая";
+let pizza = new Pizza(selectedStuffing, selectedSize);
+
+function updateAddButton() {
+  addToCartButton.innerText = `Добавить в корзину за ${pizza.calculatePrice()}₽ (${pizza.calculateCalories()} ККал)`;
+}
+
+function highlightButtons() {
+  pizzaButtons.forEach((button) => {
+    button.classList.toggle(
+      "selected",
+      button.dataset.stuffing === selectedStuffing
+    );
+  });
+
+  sizeButtons.forEach((button) => {
+    button.classList.toggle("selected", button.dataset.size === selectedSize);
+  });
+}
+
+function saveSelection() {
+  const selection = {
+    stuffing: pizza.getStuffing(),
+    size: pizza.getSize(),
+    toppings: pizza.getToppings(),
+  };
+  localStorage.setItem("pizzaSelection", JSON.stringify(selection));
+}
+
+function loadSelection() {
+  const saved = localStorage.getItem("pizzaSelection");
+  if (!saved) return;
+
+  const { stuffing, size, toppings } = JSON.parse(saved);
+
+  selectedStuffing = stuffing;
+  selectedSize = size;
+  pizza = new Pizza(stuffing, size);
+
+  toppings.forEach((toppingName) => {
+    pizza.addTopping(toppingName);
+  });
+
+  toppingCheckboxes.forEach((checkbox) => {
+    checkbox.checked = toppings.includes(checkbox.value);
+  });
+
+  highlightButtons();
+  updateAddButton();
+}
+
+pizzaButtons.forEach((button) =>
+  button.addEventListener("click", () => {
+    selectedStuffing = button.dataset.stuffing;
+    pizza = new Pizza(selectedStuffing, selectedSize);
+
+    toppingCheckboxes.forEach((checkbox) => {
+      if (checkbox.checked) pizza.addTopping(checkbox.value);
+    });
+
+    highlightButtons();
+    updateAddButton();
+    saveSelection();
+  })
+);
+
+sizeButtons.forEach((button) =>
+  button.addEventListener("click", () => {
+    selectedSize = button.dataset.size;
+    pizza = new Pizza(selectedStuffing, selectedSize);
+
+    toppingCheckboxes.forEach((checkbox) => {
+      if (checkbox.checked) pizza.addTopping(checkbox.value);
+    });
+
+    highlightButtons();
+    updateAddButton();
+    saveSelection();
+  })
+);
+
+toppingCheckboxes.forEach((checkbox) =>
+  checkbox.addEventListener("change", () => {
+    if (checkbox.checked) {
+      pizza.addTopping(checkbox.value);
+    } else {
+      pizza.removeTopping(checkbox.value);
+    }
+    updateAddButton();
+    saveSelection();
+  })
+);
+
+loadSelection();
+highlightButtons();
+updateAddButton();
