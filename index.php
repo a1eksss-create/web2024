a23d01f490aeacd22d6c1b61889586ac09cd77de
a1eksss-create <minecraftboytut@gmail.com>
2025-05-25 06:41:04 +0500

    lesson19

--- index.php
+++ index.php
@@ -1,109 +1,166 @@
+<?php
+
+function saveLog() {
+    $logFolder = 'logs';
+    $logPath = "$logFolder/log.txt";
+    
+    if (!file_exists($logFolder)) {
+        mkdir($logFolder, 0777);
+    }
+    
+    $now = date('Y-m-d H:i:s');
+    $logLine = "Request at: $now\n";
+    
+    $logLines = file_exists($logPath) ? file($logPath) : [];
+    if (count($logLines) >= 10) {
+        $fileNum = 0;
+        while (file_exists("$logFolder/log$fileNum.txt")) {
+            $fileNum++;
+        }
+        rename($logPath, "$logFolder/log$fileNum.txt");
+        file_put_contents($logPath, '');
+    }
+    
+    file_put_contents($logPath, $logLine, FILE_APPEND);
+}
+saveLog();
+
+$msg = '';
+if (isset($_FILES['image'])) {
+    $imgFolder = 'images';
+    $thumbFolder = 'thumbnails';
+    $maxFileSize = 5 * 1024 * 1024;
+    $okTypes = ['image/jpeg', 'image/png', 'image/gif'];
+    
+    if (!file_exists($imgFolder)) mkdir($imgFolder, 0777);
+    if (!file_exists($thumbFolder)) mkdir($thumbFolder, 0777);
+    
+    $file = $_FILES['image'];
+    $fileName = preg_replace('/[^A-Za-z0-9_\-\.]/', '_', basename($file['name']));
+    $fileName = substr($fileName, 0, 50);
+    $fileType = mime_content_type($file['tmp_name']);
+    $fileSize = $file['size'];
+    
+    if (!in_array($fileType, $okTypes)) {
+        $msg = 'Только JPEG, PNG или GIF';
+    } elseif ($fileSize > $maxFileSize) {
+        $msg = 'Файл больше 5Mb';
+    } else {
+        $imgPath = "$imgFolder/$fileName";
+        if (move_uploaded_file($file['tmp_name'], $imgPath)) {
+            $thumbPath = "$thumbFolder/thumb_$fileName";
+            if (makeThumb($imgPath, $thumbPath, 200, 200)) {
+                $msg = 'Картинка загружена';
+                header('Location: index.php');
+                exit;
+            } else {
+                $msg = 'Ошибка при создании миниатюры';
+            }
+        } else {
+            $msg = 'Ошибка при загрузке';
+        }
+    }
+}
+
+function makeThumb($src, $dst, $maxW, $maxH) {
+    $thumbDir = dirname($dst);
+    if (!file_exists($thumbDir)) {
+        mkdir($thumbDir, 0777, true);
+    }
+    
+    list($w, $h, $type) = getimagesize($src);
+    
+    if ($type == IMAGETYPE_JPEG) {
+        $img = imagecreatefromjpeg($src);
+    } elseif ($type == IMAGETYPE_PNG) {
+        $img = imagecreatefrompng($src);
+    } elseif ($type == IMAGETYPE_GIF) {
+        $img = imagecreatefromgif($src);
+    } else {
+        return false;
+    }
+    
+    $scale = min($maxW / $w, $maxH / $h);
+    $newW = (int)($w * $scale); 
+    $newH = (int)($h * $scale);
+    
+    $newImg = imagecreatetruecolor($newW, $newH);
+    
+    if ($type == IMAGETYPE_PNG) {
+        imagealphablending($newImg, false);
+        imagesavealpha($newImg, true);
+    }
+    
+    imagecopyresampled($newImg, $img, 0, 0, 0, 0, $newW, $newH, $w, $h);
+    
+    $result = false;
+    if ($type == IMAGETYPE_JPEG) {
+        $result = imagejpeg($newImg, $dst);
+    } elseif ($type == IMAGETYPE_PNG) {
+        $result = imagepng($newImg, $dst);
+    } elseif ($type == IMAGETYPE_GIF) {
+        $result = imagegif($newImg, $dst);
+    }
+    
+    imagedestroy($img);
+    imagedestroy($newImg);
+    return $result;
+}
+
+function showGallery($imgFolder, $thumbFolder) {
+    $exts = ['jpg', 'jpeg', 'png', 'gif'];
+    $pics = [];
+    
+    if (file_exists($imgFolder)) {
+        $files = scandir($imgFolder);
+        foreach ($files as $file) {
+            $ext = strtolower(pathinfo($file, PATHINFO_EXTENSION));
+            if (in_array($ext, $exts)) {
+                $cleanFile = preg_replace('/[^A-Za-z0-9_\-\.]/', '_', $file);
+                $cleanFile = substr($cleanFile, 0, 50);
+                if ($cleanFile != $file) {
+                    rename("$imgFolder/$file", "$imgFolder/$cleanFile");
+                    $file = $cleanFile;
+                }
+                $pics[] = $file;
+                $thumb = "$thumbFolder/thumb_$file";
+                if (!file_exists($thumb)) {
+                    makeThumb("$imgFolder/$file", $thumb, 200, 200);
+                }
+            }
+        }
+    }?>
+    
+    <div class="gallery"> <?php
+    foreach ($pics as $pic):
+        $thumb = "$thumbFolder/thumb_$pic";
+        if (file_exists($thumb)) {
+            echo "<a href='$imgFolder/$pic' target='_blank'><img src='$thumb' width='200'></a>";
+        }
+    endforeach; ?>
+    </div> <?php
+}
+?>
+
 <!DOCTYPE html>
-<html lang="ru">
+<html>
 <head>
     <meta charset="UTF-8">
-    <title>Л/р 17</title>
+    <title>Галерея</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <h3>Задание №1</h3>
+    <h1>Галерея</h1>
     
-    <?php
-    $i = 0;
-    do {
-        if ($i == 0) {
-            echo "$i – это ноль.<br>";
-        } elseif ($i % 2 == 0) {
-            echo "$i – чётное число.<br>";
-        } else {
-            echo "$i – нечётное число.<br>";
-        }
-        $i++;
-    } while ($i <= 10);
-    ?>
-
-    <h3>Задание №2</h3>
-    <?php
-    $regions = [
-    'Московская область' => ['Москва', 'Зеленоград', 'Клин'],
-    'Ленинградская область' => ['Санкт-Петербург', 'Всеволожск', 'Павловск', 'Кронштадт'],
-    'Рязанская область' => ['Рязань', 'Касимов', 'Ряжск'],
-    'Тюменская область' => ['Тюмень', 'Тобольск', 'Ишим']
-    ];
-
-    foreach ($regions as $region => $cities) {
-        echo "$region:<br>";
-        echo implode(', ', $cities) . ".<br><br>";
-    }
-    ?>
+    <form method="post" enctype="multipart/form-data">
+        <input type="file" name="image" accept="image/*" required>
+        <input type="submit" value="Загрузить">
+    </form>
     
-    <h3>Задание №3</h3>
-    <?php
-
-   function transliterate($str) {
-    $translit = [
-        'а' => 'a',   'б' => 'b',   'в' => 'v',   'г' => 'g', 'д' => 'd',   'е' => 'e',   'ё' => 'yo',  'ж' => 'zh',
-        'з' => 'z',   'и' => 'i',   'й' => 'y',   'к' => 'k', 'л' => 'l',   'м' => 'm',   'н' => 'n',   'о' => 'o',
-        'п' => 'p',   'р' => 'r',   'с' => 's',   'т' => 't', 'у' => 'u',   'ф' => 'f',   'х' => 'h',   'ц' => 'ts',
-        'ч' => 'ch',  'ш' => 'sh',  'щ' => 'sch', 'ъ' => '', 'ы' => 'y',   'ь' => "'",    'э' => 'e',   'ю' => 'yu', 'я' => 'ya'
-    ];
-
-    $str = mb_strtolower($str, 'UTF-8');
-    $result = '';
-    for ($i = 0; $i < mb_strlen($str); $i++) {
-        $char = mb_substr($str, $i, 1);
-        $result .= $translit[$char] ?? $char;
-    }
-
-    return $result;
-    }?>
-    <p>Прикладная информатика:</p> <?php
-    echo transliterate("Прикладная информатика"); 
-    ?>
-
-    <h3>Задание №4</h3>
-    <?php
-    $menu = [
-        'Главная' => '/',
-        'Тюмгу' => '/',
-        'Институты' => [
-            'ШКН' => '/',
-            'ИнХИМ' => '/',
-            'ФТИ' => '/'
-        ],
-        'Контакты' => '/'
-    ];
-
-    function dinamicMenu($menuItems) {?>
-        <ul><?php
-        foreach ($menuItems as $title => $link): ?>
-            <li> <?php
-            if (is_array($link)) {
-                echo "<a href='#'>$title</a>";
-                dinamicMenu($link);
-            } else {
-                echo "<a href='$link'>$title</a>";
-            }?>
-            </li><?php
-        endforeach; ?>
-        </ul> <?php
-    }
-    dinamicMenu($menu);
-    ?>
-
-    <h3>Задание №6</h3>
-    <p>Города на букву К:</p>
-    <?php
-    foreach ($regions as $region => $cities):
-        $filtered = array_filter($cities, function($city) {
-            return mb_substr($city, 0, 1, 'UTF-8') === 'К';
-        });
-
-        if (!empty($filtered)) {
-            echo "$region:<br>";
-            echo implode(', ', $filtered) . ".<br><br>";
-        }
-
-    endforeach
-    ?>
+    <?php if ($msg): ?>
+        <p><?= $msg; ?></p>
+    <?php endif; ?>
+    
+    <?php showGallery('images', 'thumbnails'); ?>
 </body>
 </html>
